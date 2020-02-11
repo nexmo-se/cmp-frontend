38a281244a1979d9091b787754ff0eeecedc2543
Frans Huang <franssiswanto@gmail.com>
2020-02-11 17:10:20 +0800

    fix modal, now can hide on dismiss

--- src/components/Modal/ModalHeader.js
+++ src/components/Modal/ModalHeader.js
@@ -1,10 +1,14 @@
 import React from "react";
 
-function ModalHeader({ children }){
+function ModalHeader({ children, setVisible }){
+  function handleDismiss(){
+    setVisible(false);
+  }
+
   return (
     <div className="Vlt-modal__header">
       {children}
-      <div className="Vlt-modal__dismiss"></div>
+      <div className="Vlt-modal__dismiss" onClick={handleDismiss} />
     </div>
   )
 }
