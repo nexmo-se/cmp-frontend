25fabcc9dab5291f41cb24543adfc8e26aba663b
Frans Huang <franssiswanto@gmail.com>
2020-02-14 11:42:59 +0800

    change csv header

--- src/api/csv.js
+++ src/api/csv.js
@@ -1,7 +1,9 @@
+import Papa from "papaparse";
+
 class CSVAPI{
   static generateBlaster(){
     const content = [
-      [ "recipient", "activeTime", "duration", "activeOnWeekends", "timezone" ],
+      [ "recipient", "startTime", "duration (hours)", "activeOnWeekends", "timezone" ],
       [ "6585773051", "08:00", 10, "yes", "Asia/Singapore", "VONAGE", "12345" ],
       [ "6585773061", "08:00", 10, "no", "Asia/Singapore", "VONAGE", "67890" ],
       [ "Do not remove example above. Fill your content right below this row. Please do not rename the file name as well." ]
@@ -9,6 +11,14 @@ class CSVAPI{
     const strContent = content.map((e) => e.join(",")).join("\n");
     const csvContent = `data:text/csv;charset=utf-8,${strContent}`;
     return csvContent;
+  }
+
+  static async parse(file){
+    return new Promise((resolve, reject) => {
+      Papa.parse(file, {
+        complete: (results) => resolve(results)
+      })
+    })
   }
 }
 export default CSVAPI;
