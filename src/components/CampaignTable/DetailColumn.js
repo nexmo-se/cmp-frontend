0ffe578a4d33a100e39ea405ac7ea1b9c0504763
Frans Huang <franssiswanto@gmail.com>
2020-02-28 13:10:21 +0700

    able to start campaign without any data on server

--- src/components/CampaignTable/DetailColumn.js
+++ src/components/CampaignTable/DetailColumn.js
@@ -6,16 +6,18 @@ import DownloadButton from "components/CampaignTable/DownloadButton";
 import StartButton from "components/CampaignTable/StartButton";
 import RestartButton from "components/CampaignTable/RestartButton";
 import PauseButton from "components/CampaignTable/PauseButton";
+import UploadButton from "components/CampaignTable/UploadButton";
 
 function DetailColumn({ campaign, setRefreshToken }){
   return (
     <TableColumn className="Vlt-table__cell--nowrap">
       <ButtonIcon icon="Vlt-icon-gear"/>
       <DownloadButton campaign={campaign}/>
+      <UploadButton />
       {campaign.status === "completed"?(
         <RestartButton setRefreshToken={setRefreshToken} />
-      ): campaign.status === "draft"?(
-        <StartButton setRefreshToken={setRefreshToken} />
+      ): campaign.status === "draft" || campaign.status === "paused"?(
+        <StartButton campaign={campaign} setRefreshToken={setRefreshToken}  />
       ): campaign.status === "pending"?(
         <PauseButton campaign={campaign} setRefreshToken={setRefreshToken} />
       ): null}
