8912cad3f94db639d54e3f5ec8a7c7620e472e7a
Frans Huang <franssiswanto@gmail.com>
2020-03-30 23:01:49 +0700

    add pagination to campaigns

--- src/components/CampaignTable/NormalTable.js
+++ src/components/CampaignTable/NormalTable.js
@@ -2,6 +2,7 @@ import React from "react";
 import moment from "moment";
 
 import NumberIndicator from "components/NumberIndicator";
+import Pagination from "components/Pagination";
 import Table from "components/Table";
 import TableHead from "components/Table/TableHead";
 import TableRow from "components/Table/TableRow";
@@ -13,49 +14,59 @@ import TableBodyRow from "components/Table/TableBodyRow";
 import DetailColumn from "./DetailColumn";
 
 function NormalTable({ campaigns, setRefreshToken, limit=10 }){
-  return (
-    <Table>
-      <TableHead>
-        <TableRow>
-          <TableHeader />
-          <TableHeader>NAME</TableHeader>
-          <TableHeader>START DATE</TableHeader>
-          <TableHeader>END DATE</TableHeader>
-          <TableHeader>END DATE</TableHeader>
-          <TableHeader />
-        </TableRow>
-      </TableHead>
-      <TableBody>
-        {campaigns.slice(0, limit + 1).map((campaign, index) => {
-          let badgeBackground = "Vlt-grey";
-          if(campaign.status === "pending" || campaign.status === "paused") badgeBackground = "Vlt-yellow";
-          else if(campaign.status.started || campaign.status === "completed") badgeBackground = "Vlt-green";
-          
-          const startDate = new moment(campaign.campaignStartDate).local().format("DD MMMM YYYY");
-          const endDate = new moment(campaign.campaignEndDate).local().format("DD MMMM YYYY");
+  const [ currentPage, setCurrentPage ] = React.useState(1);
 
-          return(
-            <TableBodyRow key={campaign.id}>
-              <TableColumn>
-                <NumberIndicator number={index + 1} />
-              </TableColumn>
-              <TableColumn>
-                <p>
-                  <b>
-                    {campaign.name} &nbsp;|&nbsp;
-                    <span className={badgeBackground}>{campaign.status}</span>
-                  </b>
-                </p>
-                <p className="Vlt-grey Vlt-truncate" style={{ maxWidth: 300 }}>{campaign.id}</p>
-              </TableColumn>
-              <TableColumn>{startDate}</TableColumn>
-              <TableColumn>{endDate}</TableColumn>
-              <DetailColumn campaign={campaign} setRefreshToken={setRefreshToken} />
-            </TableBodyRow>
-          )
-        })}
-      </TableBody>
-    </Table>
+  return (
+    <React.Fragment>
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableHeader />
+            <TableHeader>NAME</TableHeader>
+            <TableHeader>START DATE</TableHeader>
+            <TableHeader>END DATE</TableHeader>
+            <TableHeader>END DATE</TableHeader>
+            <TableHeader />
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {campaigns.slice((currentPage - 1) * limit, limit * currentPage).map((campaign, index) => {
+            let badgeBackground = "Vlt-grey";
+            if(campaign.status === "pending" || campaign.status === "paused") badgeBackground = "Vlt-yellow";
+            else if(campaign.status.started || campaign.status === "completed") badgeBackground = "Vlt-green";
+            
+            const startDate = new moment(campaign.campaignStartDate).local().format("DD MMMM YYYY");
+            const endDate = new moment(campaign.campaignEndDate).local().format("DD MMMM YYYY");
+            const number = ((currentPage - 1) * limit) + index + 1
+
+            return(
+              <TableBodyRow key={campaign.id}>
+                <TableColumn>
+                  <NumberIndicator number={number} />
+                </TableColumn>
+                <TableColumn>
+                  <p>
+                    <b>
+                      {campaign.name} &nbsp;|&nbsp;
+                      <span className={badgeBackground}>{campaign.status}</span>
+                    </b>
+                  </p>
+                  <p className="Vlt-grey Vlt-truncate" style={{ maxWidth: 300 }}>{campaign.id}</p>
+                </TableColumn>
+                <TableColumn>{startDate}</TableColumn>
+                <TableColumn>{endDate}</TableColumn>
+                <DetailColumn campaign={campaign} setRefreshToken={setRefreshToken} />
+              </TableBodyRow>
+            )
+          })}
+        </TableBody>
+      </Table>
+      <Pagination 
+        totalData={campaigns.length} 
+        limit={limit} 
+        onChange={setCurrentPage}
+      />
+    </React.Fragment>
   )
 }
 export default NormalTable;
---
+++ src/components/Pagination/index.js
@@ -0,0 +1,60 @@
+import React from "react";
+
+import Number from "./Number";
+
+function Pagination({ totalData, limit, onChange }){
+  const [ currentPage, setCurrentPage ] = React.useState(1);
+  const [ totalPage, setTotalPage ] = React.useState(null);
+
+  function handleGoTo(number){
+    setCurrentPage(number);
+  }
+
+  function handleNextClick(){
+    setCurrentPage((prevCurrentPage) => {
+      if(prevCurrentPage + 1 <= totalPage){
+        return prevCurrentPage + 1;
+      }
+    });
+  }
+
+  function handlePrevClick(){
+    setCurrentPage((prevCurrentPage) => {
+      if(prevCurrentPage - 1 >= 0){
+        return prevCurrentPage - 1;
+      }
+    })
+  }
+
+  React.useEffect(() => {
+    setTotalPage(Math.ceil(totalData / limit));
+  }, [ totalData ]);
+
+  React.useEffect(() => {
+    if(onChange) onChange(currentPage);
+  }, [ currentPage ])
+
+  return (
+    <div className="Vlt-table__pagination">
+      <ul>
+        <li className="Vlt-table__pagination__prev">
+          <a onClick={handlePrevClick}>Previous</a>
+        </li>
+        {[...Array(totalPage)].map((_, index) => {
+          const number = index + 1;
+          return (
+            <Number 
+              number={number}
+              selected={currentPage === number}
+              onClick={() => handleGoTo(number)}
+            />
+          )
+        })}
+        <li className="Vlt-table__pagination__next">
+          <a onClick={handleNextClick}>Next</a>
+        </li>
+      </ul>
+    </div>
+  )
+}
+export default Pagination;
